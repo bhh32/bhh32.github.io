ef3afe3c510e59aaeb195a7846d21b9779cd34a0
Bryan Hyland <bryan.hyland32@gmail.com>
2019-06-21 23:12:13 -0700

    Added JS to detect Internet Explorer and popup a message stating it's not supported

--- js/cpp_js.js
+++
@@ -1,8 +0,0 @@
-function formSubmit(form) {
-  var lName = document.getElementById("lname").value;
-  var fName = document.getElementById("fname").value;
-
-  alert(fName + " " + lName + " you have been added to the mailing list!");
-
-  return false;
-}
---
+++ js/iedetect.js
@@ -0,0 +1,8 @@
+// Internet Explorer 6-11
+var isIE = /*@cc_on!@*/false || !!document.documentMode;
+
+if (isIE) {
+  window.alert("This page is not supported by Internet Explorer and may not display properly.\n\n"
+                + "If you'd like to view the page with all of its rich features and content "
+                + "please use Mozilla Firefox, Google Chrome, Opera, or Microsoft Edge.");
+}
